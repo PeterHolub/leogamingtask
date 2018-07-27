d85dd40dac22e2bb667fffbfbdcb3eddb9c934a5
peterholub <holub.petr@hotmail.com>
2018-07-27 12:10:35 +0300

    quick update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.security.GeneralSecurityException;
 import java.security.SignatureException;
+
 import service.EncoderService;
 import service.KeyReaderService;
 
@@ -11,39 +12,42 @@ public class Main {
 
     }
 
-    public void makeTest() throws  SignatureException {
+    public void makeTest() throws SignatureException {
 
         KeyReaderService keyReaderService = new KeyReaderService();
         EncoderService encoderService = new EncoderService(keyReaderService);
 
-        String XmlRequest = "<request point=\"327\"> \n" +
-                "<verify service=\"4390\" account=\"12345\"/> \n" +
+        String xmlRequest = "<request point=\"327\">\n" +
+                "<verify service=\"4390\" account=\"12345\"></verify>\n" +
                 "</request>";
 
         System.out.println("Encoding request with Private Key to create signature");
 
-        String signature = encoderService.sign(XmlRequest,new File("src/main/resources/private.pem"));
 
+        String signature = encoderService.sign(xmlRequest, new File("src/main/resources/private.pem"));
+        System.out.println(signature);
         System.out.println("Verify signature with public.pem");
 
-        boolean check = encoderService.verify(XmlRequest, signature,new File("src/main/resources/public.pem"));
+        boolean check = encoderService.verify(xmlRequest, signature, new File("src/main/resources/public.pem"));
 
         System.out.println("Result of verification with public.pem: " + check);
 
-        System.out.println("Verify signature with GeneratedFromPrivateKey.pem");
+        System.out.println("Verify signature with generatedFromPrivateKey.pem");
 
-        boolean check2 = encoderService.verify(XmlRequest, signature,new File("src/main/resources/GeneratedFromPrivateKey.pem"));
-        System.out.println("Result of verification with public.pem: " + check2);
+        boolean check2 = encoderService.verify(xmlRequest, signature, new File("src/main/resources/generatedFromPrivateKey.pem"));
+        System.out.println("Result of verification with generatedFromPrivateKey.pem: " + check2);
 
 
-        System.out.println("Encoding request with randomPrivate Key to create signature");
+        System.out.println("Encoding request with randomPrivate.pem to create signature");
 
-        String signature2 = encoderService.sign(XmlRequest,new File("src/main/resources/randomPrivate.pem"));
+        String signature2 = encoderService.sign(xmlRequest, new File("src/main/resources/randomPrivate.pem"));
+
+        System.out.println(signature2);
         System.out.println("Verify signature with randomPublic.pem");
 
-        boolean check3 = encoderService.verify(XmlRequest, signature2 ,new File("src/main/resources/randomPublic.pem"));
+        boolean check3 = encoderService.verify(xmlRequest, signature2, new File("src/main/resources/randomPublic.pem"));
 
         System.out.println("Result of verification with public.pem: " + check3);
 
-  }
+    }
 }
